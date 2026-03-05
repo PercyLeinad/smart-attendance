24012e6cd00bf75cd5b047b8f85ab47db6671581
Percy Daniel <102838909+PercyLeinad@users.noreply.github.com>
2026-03-05 14:04:28 +0300

    fix: update comment for local IP address retrieval in launch script

--- launch.sh
+++ launch.sh
@@ -6,6 +6,7 @@ source $(pwd)/.venv/bin/activate
 
 cd app
 
+# Get the local IP address of the machine changed to wlp2s0 for wireless, change to eth0 for wired connection
 IP=$(ip -4 -o addr show wlp2s0 | awk '{print $4}' | cut -d/ -f1)
 
 uvicorn main:app --reload --port 8000 --host "$IP"
