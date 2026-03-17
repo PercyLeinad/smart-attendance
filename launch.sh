241a86aedcd33e1242d8e94a93a41b8b762eb433
Daniel Mutwiri <102838909+PercyLeinad@users.noreply.github.com>
2026-03-17 14:21:15 +0300

    Set IP to localhost in launch script
    Removed automatic IP retrieval and set IP to localhost.

--- launch.sh
+++ launch.sh
@@ -7,9 +7,6 @@ source $(pwd)/.venv/bin/activate
 
 cd app
 
-# Get machine IP automatically
-#IP=$(ip route get 1.1.1.1 | awk '{print $7; exit}')
-
 IP=127.0.0.1
 
 uvicorn main:app --reload --port 8000 --host "$IP"
