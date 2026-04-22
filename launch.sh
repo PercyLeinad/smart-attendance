18898bf2dc5bd5249052ef39cf9376c315289f5c
Percy Daniel <102838909+PercyLeinad@users.noreply.github.com>
2026-04-22 12:48:46 +0300

    added gunicorn logs

--- launch.sh
+++ launch.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Starting FastAPI with Gunicorn..."
-
 source "$(pwd)/.venv/bin/activate"
 
 IP=127.0.0.1
 PORT=8000
 WORKERS=8
 
+echo "Starting FastAPI with Gunicorn..."
+
 gunicorn app.main:app \
     -k uvicorn.workers.UvicornWorker \
     -w "$WORKERS" \
     -b "$IP:$PORT" \
-    --timeout 30
+    --timeout 30 \
+    --access-logfile "$LOG_DIR/access.log" \
+    --error-logfile "$LOG_DIR/error.log" \
+    --log-level info
