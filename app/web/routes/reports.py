9791544dfaf29fc6335c03a4a3d5b8225d786354
Percy Daniel <102838909+PercyLeinad@users.noreply.github.com>
2026-04-27 09:01:48 +0300

    feat: add device risk report endpoints and CSV export functionality

--- app/web/routes/reports.py
+++ app/web/routes/reports.py
@@ -4,16 +4,28 @@ from datetime import datetime, date
 from pathlib import Path
 import pandas as pd
 from app.web.routes.auth import is_admin
-from app.services.reports import get_attendance_report
+from app.services.reports import get_attendance_report, get_device_risk_report
+from app.schemas.device import DeviceRiskReportResponse
 
 router = APIRouter(
     dependencies=[Depends(is_admin)]
 )
+# -----------------------------
+# Get Report Data (for UI)
+# -----------------------------
+@router.get("/admin/report/attendance")
+def report_by_range(
+    start_date: date = Query(...),
+    end_date: date = Query(...)
+):
+    # 🔥 Same service reused
+    data = get_attendance_report(start_date, end_date)
+    return data
 
 # -----------------------------
 # Export CSV Report
 # -----------------------------
-@router.get("/admin/report/export")
+@router.get("/admin/report/attendance/export")
 def export_data(
     start_date: date = Query(...),
     end_date: date = Query(...)
@@ -42,14 +54,41 @@ def export_data(
     )
 
 
-# -----------------------------
-# Get Report Data (for UI)
-# -----------------------------
-@router.get("/admin/report")
-def report_by_range(
-    start_date: date = Query(...),
-    end_date: date = Query(...)
-):
-    # 🔥 Same service reused
-    data = get_attendance_report(start_date, end_date)
-    return data
+@router.get(
+    "/admin/report/device-risk",
+    response_model=DeviceRiskReportResponse
+)
+def device_risk_report(start_date: date, end_date: date):
+    data =  get_device_risk_report(start_date, end_date)
+    return { "data": data }
+    
+
+@router.get(
+    "/admin/report/device-risk/export",
+    response_model=DeviceRiskReportResponse
+)
+def device_risk_report_export(start_date: date, end_date: date):
+    data =  get_device_risk_report(start_date, end_date)
+    df = pd.DataFrame(data)
+
+    # Ensure reports folder exists
+    reports_folder = Path("reports")
+    reports_folder.mkdir(exist_ok=True)
+
+    # Generate filename
+    today_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"device_risk_report_{today_str}.csv"
+    file_path = reports_folder / filename
+
+    # Save CSV
+    df.to_csv(file_path, index=False)
+
+    return FileResponse(
+        path=file_path,
+        media_type="text/csv",
+        filename=filename
+    )
+
+if '__name__' == '__main__':
+    reports_folder = Path("reports")
+    print(reports_folder)
